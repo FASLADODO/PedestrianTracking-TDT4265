a34a15a4b9d02bce1f25ab1d039b129598fc68ac
Patrick Feiring <patrickf@ntnu.no>
2016-04-10 10:34:47 +0200

    Basic Kalman filtering.

--- Pedestrian.m
+++ Pedestrian.m
@@ -1,9 +1,14 @@
 classdef Pedestrian < handle
-    properties
+   
+    properties (Access = private)
+        
         position;
-        measurements;
         velocity;
-        state;
+        covariance;
+        
+        state;          % INITIALIZATION | ACTIVE
+        
+        measurements;
     end
     
     methods
@@ -11,9 +16,13 @@ classdef Pedestrian < handle
         % Constructor
         
         function obj = Pedestrian(measurement)
-            obj.measurements = {};
+            
             obj.position = measurement.position;
             obj.velocity = [0; 0];
+            
+            obj.covariance = diag([1 1 5 5]);       % Unsure about speed of target
+            
+            obj.measurements = {};
             obj.measurements{1} = measurement;
         end
         
@@ -40,6 +49,47 @@ classdef Pedestrian < handle
             rectangle('Position', configuration, 'EdgeColor', 'b');
         end
         
+        % Kalman filtering
+        
+        function kalman_prediction(obj, pedestrian_motion_model)
+            
+            F = pedestrian_motion_model.F;
+            Q = pedestrian_motion_model.Q;
+            
+            x = [obj.position; obj.velocity];
+            P = obj.covariance;
+            
+            x = F * x;
+            P = F * P * F';
+            
+            obj.position = x(1:2);
+            obj.velocity = x(3:4);
+            obj.covariance = P;
+        end
+        
+        function kalman_update(obj, pedestrian_motion_model)
+            
+            latest_measurement = obj.get_latest_measurement();
+            
+            H = pedestrian_motion_model.H;
+            R = pedestrian_motion_model.R;
+            
+            x = [obj.position; obj.velocity];
+            P = obj.covariance;
+            z = latest_measurement.position;
+            
+            y = (z - H * x);
+            S = H * P * H' + R;
+            K = P * H' * inv(S);
+            I = eye(length(x));
+            
+            x = x + K * y;
+            P = (I - K * H) * P;
+            
+            obj.position = x(1:2);
+            obj.velocity = x(3:4);
+            obj.covariance = P;
+        end
     end
     
 end
---
+++ PedestrianMotionModel.m
@@ -0,0 +1,39 @@
+classdef PedestrianMotionModel
+  
+    properties (Access = private)
+        timestep;
+    end
+    
+    properties (Access = public)
+        
+        F;
+        H;
+            
+        Q;
+        R;
+    end
+    
+    methods
+        
+        % Constructor
+        
+        function obj = PedestrianMotionModel()
+            
+            obj.timestep = 1;
+
+            obj.F = [   1   0   obj.timestep    0;
+                        0   1   0               obj.timestep;
+                        0   0   1               0;
+                        0   0   0               1];
+            
+            obj.H = [   1   0   0               0;
+                        0   1   0               0];
+
+            obj.Q = diag([  1   1   5   5]);
+            obj.R = diag([  1   1]);
+            
+        end
+    end
+    
+end
+
