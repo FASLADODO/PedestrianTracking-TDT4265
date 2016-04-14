f4df66d98ce83ae2c32ff5a950d2640c95c5fa76
Roy Konrad Angelsen <royka@ntnu.no>
2016-04-14 14:01:35 +0200

    Renamed classes

---
+++ Pedestrian_motion_model.m
@@ -0,0 +1,46 @@
+classdef Pedestrian_motion_model
+  
+    properties (Access = private)
+        timestep;
+    end
+    
+    properties (Access = public)
+        
+        F;
+        H;
+            
+        Q;
+        R;
+    end
+    
+    methods
+        
+        %% Constructor
+        
+        function obj = Pedestrian_motion_model()
+            
+            obj.timestep = 1;
+
+            % Constant velocity motion model
+            %
+            % State:
+            %   x
+            %   y
+            %   x speed
+            %   y speed
+            
+            obj.F = [   1   0   obj.timestep    0;
+                        0   1   0               obj.timestep;
+                        0   0   1               0;
+                        0   0   0               1];
+            
+            obj.H = [   1   0   0               0;
+                        0   1   0               0];
+
+            obj.Q = 0.005 * diag([  3   3   5  5]);
+            obj.R = 5    * diag([  2   2]);
+        end
+    end
+    
+end
+
